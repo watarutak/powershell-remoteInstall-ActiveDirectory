0f46449d2ea9617cb72efca32af095dcf269f4cc
watarutak <wataru.takasaki@gmail.com>
2016-11-14 20:05:42 +0900

    add installation.ps1

---
+++ installation.ps1
@@ -0,0 +1,7 @@
+﻿# Args[0]  installer name
+# Args[1]  silent install option
+
+$path = "./" + $Args[0]
+$process = (Start-Process -FilePath $path -ArgumentList $Args[1] -Wait)
+
+Exit $process.ExitCode
